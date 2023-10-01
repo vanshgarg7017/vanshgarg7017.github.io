f0228a827521820338433f0810a6dd756e6aff40
Vansh Garg <vanshgarg7017@gmail.com>
2023-10-01 11:35:53 +0530

    Skills section edit and remove some sections

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const hamburger = document.getElementsByClassName("hamburger")[0]
 const mobileNavs = document.getElementsByClassName("nav-links")[0]
+const panels = document.querySelectorAll('.panel')
 hamburger.addEventListener("click", () => {
   mobileNavs.classList.toggle("active")
 })
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
