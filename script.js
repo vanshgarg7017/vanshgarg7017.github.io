0d938e9b8923d832e1153d2f6d5b97b4c019c424
Vansh Garg <vanshgarg7017@gmail.com>
2023-10-03 00:04:38 +0530

    some Changes in javascript code

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const hamburger = document.getElementsByClassName("hamburger")[0]
-const mobileNavs = document.getElementsByClassName("nav-links")[0]
+// const hamburger = document.getElementsByClassName("hamburger")[0]
+// const mobileNavs = document.getElementsByClassName("nav-links")[0]
 const panels = document.querySelectorAll('.panel')
-hamburger.addEventListener("click", () => {
-  mobileNavs.classList.toggle("active")
-})
+// hamburger.addEventListener("click", () => {
+//   mobileNavs.classList.toggle("active")
+// })
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
